8306ff9df56dbbd36b4ddc0b60215711db18d05c
NUR ADLINA BALKIS BINTI MUHAMMAD RAZALI <balkisadlina@gmail.com>
2025-12-26 20:04:26 +0800

    edit roles

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './team.css'
-import adlina from '../../assets/program-1.jpg'
-import auni from '../../assets/program-2.jpg'
+import adlina from '../../assets/adlina.jpeg'
+import auni from '../../assets/Auni.jpg'
 import insyirah from '../../assets/program-3.jpg'
 
 const Team = () => {
@@ -11,6 +11,11 @@ const Team = () => {
             <img src={adlina} alt="Adlina"/> 
             <div className="caption">
                 <p>Nur Adlina Balkis Binti Muhammad Razali</p>
+                <ul>
+                    <li>Web & Digital Solutions Developer</li>
+                    <li>Design, develop, and maintain responsive websites</li>
+                    <li>Implement modern UI?UX practices to enhance user experience</li>
+                </ul>
             </div>
         </div>
 
@@ -18,6 +23,11 @@ const Team = () => {
             <img src={auni} alt="Auni"/>
             <div className="caption">
                 <p>Auni Amira Binti Md Fauzi</p>
+                <ul>
+                    <li>Cloud & Database Solution Engineer</li>
+                    <li>Design, develop, and manage cloud-based infrastructure and service</li>
+                    <li>Implement data backup, recovery, and access control strategies</li>       
+                </ul>         
             </div>
         </div>
         
@@ -25,10 +35,14 @@ const Team = () => {
             <img src={insyirah} alt="Insyirah"/> 
             <div className="caption">
                 <p>Nur Insyirah Binti Mohamad Azmi</p>
+                <ul>
+                    <li>CYbersecurity & SYstems Maintenance Specialist</li>
+                    <li>Implement and monitor cybersecurity measures to protect systems and data</li>
+                    <li>Respond to security incidents and provide preventive solutions</li>       
+                </ul>
             </div>
         </div>
     </div>
   )
 }
-
 export default Team
