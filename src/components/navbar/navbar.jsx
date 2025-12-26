09a7b73edafa0cb8ba2448f30d2baf35feafd526
NUR ADLINA BALKIS BINTI MUHAMMAD RAZALI <balkisadlina@gmail.com>
2025-12-27 06:48:02 +0800

    enlarge icon +function

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -21,16 +21,28 @@ const Navbar = () => {
     setMobileMenu(!mobilemenu);
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-      <img src={logo} alt="Logo" className='logo'/>
+      <img 
+        src={logo} 
+        alt="Logo" 
+        className='logo' 
+        onClick={scrollToTop}
+      />
 
       <ul className={mobilemenu ? '' : 'hidemobilemenu'}>
-        <li><Link to='members' smooth offset={0} duration={500}>Home</Link></li>
-        <li><Link to='services' smooth offset={-120} duration={500}>Services</Link></li>
-        <li><Link to='about' smooth offset={-120} duration={500}>About us</Link></li>
-        <li><Link to='team-section' smooth offset={-120} duration={500}>Team Members</Link></li>
-        <li><Link to='contact' smooth offset={-120} duration={500} className='btn'>Contact us</Link></li>
+        <li><Link to='members' smooth offset={0} duration={500} onClick={() => setMobileMenu(false)}>Home</Link></li>
+        <li><Link to='services' smooth offset={-120} duration={500} onClick={() => setMobileMenu(false)}>Services</Link></li>
+        <li><Link to='about' smooth offset={-120} duration={500} onClick={() => setMobileMenu(false)}>About us</Link></li>
+        <li><Link to='team-section' smooth offset={-120} duration={500} onClick={() => setMobileMenu(false)}>Team Members</Link></li>
+        <li><Link to='contact' smooth offset={-120} duration={500} className='btn' onClick={() => setMobileMenu(false)}>Contact us</Link></li>
       </ul>
 
       <img src={menuicon} alt="Menu" className='menuicon' onClick={toggleMenu}/>
