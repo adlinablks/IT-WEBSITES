c3f4ba60edf0bc8f5b880a3153b4062a82a43d75
NUR ADLINA BALKIS BINTI MUHAMMAD RAZALI <balkisadlina@gmail.com>
2025-12-26 21:08:11 +0800

    gambar insyirah

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './team.css'
 import adlina from '../../assets/adlina.jpeg'
 import auni from '../../assets/Auni.jpg'
-import insyirah from '../../assets/program-3.jpg'
+import insyirah from '../../assets/insyirah.jpeg'
 
 const Team = () => {
   return (
@@ -20,18 +20,6 @@ const Team = () => {
         </div>
 
         <div className="member">
-            <img src={auni} alt="Auni"/>
-            <div className="caption">
-                <p>Auni Amira Binti Md Fauzi</p>
-                <ul>
-                    <li>Cloud & Database Solution Engineer</li>
-                    <li>Design, develop, and manage cloud-based infrastructure and service</li>
-                    <li>Implement data backup, recovery, and access control strategies</li>       
-                </ul>         
-            </div>
-        </div>
-        
-        <div className="member">
             <img src={insyirah} alt="Insyirah"/> 
             <div className="caption">
                 <p>Nur Insyirah Binti Mohamad Azmi</p>
@@ -40,6 +28,19 @@ const Team = () => {
                     <li>Implement and monitor cybersecurity measures to protect systems and data</li>
                     <li>Respond to security incidents and provide preventive solutions</li>       
                 </ul>
+            </div>
+        </div>
+
+
+        <div className="member">
+            <img src={auni} alt="Auni"/>
+            <div className="caption">
+                <p>Auni Amira Binti Md Fauzi</p>
+                <ul>
+                    <li>Cloud & Database Solution Engineer</li>
+                    <li>Design, develop, and manage cloud-based infrastructure and service</li>
+                    <li>Implement data backup, recovery, and access control strategies</li>       
+                </ul>         
             </div>
         </div>
     </div>
