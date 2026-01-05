f1eea798a893b8399005e8ca054e8f5cb76c51a6
NUR ADLINA BALKIS BINTI MUHAMMAD RAZALI <balkisadlina@gmail.com>
2026-01-05 20:49:42 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Apply background image to body
 document.body.style.backgroundSize = 'cover'
 document.body.style.backgroundPosition = 'center'
 document.body.style.backgroundAttachment = 'fixed'
